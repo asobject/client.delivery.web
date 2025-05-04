d7fe44e88f4231e7b885e7ebb218e4508a8dcc99
Алексей Михайленко <huntxrez@gmail.com>
2025-05-04 21:36:41 +0700

    added chat

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {Card} from 'primeng/card';
+import {Button} from "primeng/button";
 
 @Component({
   selector: 'app-about',
-  imports: [
-    Card
-  ],
+    imports: [
+        Card,
+        Button
+    ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
